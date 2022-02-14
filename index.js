3f7ce66f88560d57ae32c5f5373586782ee6568f
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-02-14 16:59:05 +0100

    fix: update plugin with core rules

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const path = require('path')
+const { dirname } = require('path')
+
 module.exports = {
   prefix: 'mdi',
   regex: {
-    code: /(mdi)(\/{1}[a-z\-]+){1,2}/gm,
-    lib: /([@[a-zA-Z_\-\/]+\/([0-9a-zA-Z_-]+)\.svg)/,
+    code: /(mdi)(\/{1}[a-z-]+){1,2}/gm,
+    lib: /([@[a-zA-Z_\-/]+\/([0-9a-zA-Z_-]+)\.svg)/,
   },
   selector: path => `mdi/${path[2].replace(/[_]+/g, '-')}`,
-  path: `${path.dirname(require.resolve('@mdi/svg/package.json'))}/**/*.svg`,
+  path: `${dirname(require.resolve('@mdi/svg/package.json'))}/**/*.svg`,
 }
